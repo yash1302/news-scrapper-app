bc42a020a34971a739545deae05b25254a75f5b4
Durvesh <157624595+Durvesh-09@users.noreply.github.com>
2024-01-27 17:10:53 +0530

    def bing

--- tp.py
+++ tp.py
@@ -1,35 +1,35 @@
-# mb-15 reg searchCenterMiddle
+def bing(self):
 
-import json
-import bs4
-import requests
-
-title = []
-# heading = []
-# link = []
-# days = []
-
-response = requests.get('https://in.news.search.yahoo.com/search;_ylt=AwrPrHDa8bFlRA4EagfAHAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BhZ2luYXRpb24-?q=microsoft+AI&b=01&pz=10&xargs=0')
-soup = bs4.BeautifulSoup(response.text, 'lxml')
-
-# print(soup)
-
-class tp:
-
-    def appendNews():
-        global title
-        news = soup.find_all('div',attrs={'class':"dd NewsArticle"})
-        for i in news:
-            title.append(i.find('h4', attrs={'class':'s-title fz-16 lh-20'}).text)
-            # heading.append(i.find('span',attrs={'class':'s-source mr-5 cite-co'}).text)
-            # days.append(i.find('span',attrs={'class':'fc-2nd s-time mr-8'}).text)
-            # link.append(i.find('h4', class_='s-title fz-16 lh-20').a['href'])
-
-    appendNews()
-    print(title)
-
-# for i in link:
-#     print(i)
-
-print("hello")
     
+    inputList = self.read_config()
+    global title
+    global heading
+    global link
+    global days
+    
+    try:
+        for i,j,k in inputList:
+            response = requests.get(f"https://www.bing.com/news/search?q={i}+{j}urlnews/infinitescrollajax?page={k}")
+            soup = BeautifulSoup(response.text, 'lxml')
+                       
+            news = soup.find_all('div',attrs={'class':"caption"})
+
+            
+
+            for i in news:
+                title.append(i.find('a', attrs={'class':'title'}).text)
+                heading.append(i.find('div',attrs={'class':'source set_top'}).text)
+                days.append(i.find('span',attrs={'tabindex':'0'}).text)
+                link.append(i.find('a', class_='title').get('href'))
+                
+            ##print("hello")
+
+##            for i in range(len(title)):
+##                
+##                print(title[i])
+##                print(link[i])
+##                print(heading[i])
+##                print(days[i])
+
+    except :
+        logging.error("Error has occured")
